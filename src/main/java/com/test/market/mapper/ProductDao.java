c70739a25dcf34bc32c00b8a942a7b5e94e91e2c
Yyang2009 <479048184@qq.com>
2020-04-23 15:05:59 +0800

    some question

--- src/main/java/com/test/market/mapper/ProductDao.java
+++ src/main/java/com/test/market/mapper/ProductDao.java
@@ -1,12 +1,8 @@
 package com.test.market.mapper;
 
 import com.test.market.bean.ProductPo;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Component;
 
-@Component
-@Mapper
 public interface ProductDao {
     // 获取产品
     public ProductPo getProduct(Long id);
--- src/main/java/com/test/market/mapper/PurchaseRecordDao.java
+++ src/main/java/com/test/market/mapper/PurchaseRecordDao.java
@@ -1,11 +1,7 @@
 package com.test.market.mapper;
 
 import com.test.market.bean.PurchaseRecordPo;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
 
-@Component
-@Mapper
 public interface PurchaseRecordDao {
     public int insertPuerchaseRecord(PurchaseRecordPo pr);
 }
